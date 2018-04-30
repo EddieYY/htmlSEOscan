b6c2275fbe73e0c01ee46173ea7a4479d4962d02
Eddie Chen <yenyu0304@gmail.com>
2018-04-30 10:07:06 +0000

    modify file

--- test/test.js
+++ test/test.js
@@ -1,20 +1,24 @@
-const { htmlCheck, FileInput, ConsoleOutput } = require('../src/package')
+const { htmlCheck, FileInput, StreamInput, ConsoleOutput, FileOutput, StreamOutput } = require('../src/package')
  
 
-const InputPath = __dirname + '/test.html'
-const OutputPath = __dirname + '/out.log'
-
+const InputPath = __dirname + '/test.html' // input local file path 
 const input = new FileInput(InputPath)
-const output = new ConsoleOutput()
+
+const output = new ConsoleOutput() // ouput in Console.
+// if write output to local file
+// const OutputPath = __dirname + '/out.log'
+// const output = new FileOutput(OutputPath)
+
+// 
+// const output = new ConsoleOutput() 
+
+
  
 const htmlSEOcheck = new htmlCheck(true)
- 
 htmlSEOcheck.setInput(input)
 htmlSEOcheck.setOutput(output)
  
 htmlSEOcheck.run().then(function(result){
          htmlSEOcheck.getResult()
 })
-
-
 
